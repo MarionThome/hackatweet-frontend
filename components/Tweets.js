987737d554877e98754fee8a14bb45f25038010b
Julien Goudard <juliengoudard.g@gmail.com>
2022-11-24 17:56:43 +0100

    padding css

--- components/Tweets.js
+++ components/Tweets.js
@@ -12,7 +12,7 @@ import Trends from "./Trends";
 
 
 function Tweets() {
-  const [tweetToPost, setTweetToPost] = useState ('')
+  const [tweetToPost, setTweetToPost] = useState('')
   const login = useSelector((state) => state.users.value);
   console.log(login)
   return (
@@ -21,13 +21,18 @@ function Tweets() {
         <LeftSection />
       </div>
       <div className={styles.middleSection}>
-        <h2>Home</h2>
-        <div className={styles.tweetPostContainer}>
-          <input type="text-area"  onChange={(e) => setTweetToPost(e.target.value)} value={tweetToPost} placeholder="Post something and try to be nice and respectfull" />
-          <div className={styles.posterBottom}>
-            <span className={styles.stringCounter}>{tweetToPost.length}/280</span>
-            <div className={styles.btnTweet}>Tweet</div>
+        <div className={styles.middleHead}>
+          <h2>Home</h2>
+          <div className={styles.tweetPostContainer}>
+            <input type="text-area" onChange={(e) => setTweetToPost(e.target.value)} value={tweetToPost} placeholder="Post something and try to be nice and respectfull" />
+            <div className={styles.posterBottom}>
+              <span className={styles.stringCounter}>{tweetToPost.length}/280</span>
+              <div className={styles.btnTweet}>Tweet</div>
+            </div>
           </div>
+        </div>
+        <div className={styles.tweetContainer}>
+          
         </div>
       </div>
       <div className={styles.rightSection}>
